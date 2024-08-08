4b0bf9921adcb93ada356ca684c610549445272a
blue_sea <95969488+111jjj111@users.noreply.github.com>
2024-08-09 05:43:16 +0900

    외부디비연결

--- backend/sql_app/database.py
+++ backend/sql_app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/gdsc"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@113.198.230.24:318/summer"
+#"mysql+pymysql://root:@localhost:3306/gdsc"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,6 +14,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+    print(os.getenv("SQLALCHEMY_DATABASE_URL"))
     try:
         yield db
     finally:
