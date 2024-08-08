9c7ae4c4bbe66c6ee1049de8f81a60208fe63d38
blue_sea <95969488+111jjj111@users.noreply.github.com>
2024-08-09 05:18:27 +0900

    add dotenv

--- data/adddata.py
+++ data/adddata.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 # CSV 파일을 DataFrame으로 읽기
-df = pd.read_csv('backend/data/clothing.csv')
+df = pd.read_csv('./clothing.csv')
 
 # MySQL 데이터베이스 연결
-engine = create_engine('mysql+pymysql://root:@localhost:3306/gdsc')
+engine = create_engine('mysql+pymysql://root:1234@113.198.230.24:318/gdsc')
 
 # DataFrame을 MySQL 테이블로 저장
 df.to_sql('clothing', con=engine, if_exists='append', index=False)
